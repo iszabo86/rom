17bc0f340b6ad7bfa38e3295f65482d92ffd7410
Istvan Szabo <istvan.szabo.86@gmail.com>
2024-06-14 21:12:53 +0200

    Creating empty project

---
+++ src/main/java/com/hotel/booking/RomApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RomApplication.class, args);
+    }
+
+}
