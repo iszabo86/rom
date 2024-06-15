12c079091d0ceafe9a8de5a152952ad1b3e29859
Istvan Szabo <istvan.szabo.86@gmail.com>
2024-06-15 11:48:42 +0200

    RoomOptimizationController to handle occupancy optimization requests

--- src/main/java/com/hotel/booking/room/controller/RoomOptimizationController.java
+++ src/main/java/com/hotel/booking/room/controller/RoomOptimizationController.java
@@ -2,6 +2,12 @@ package com.hotel.booking.room.controller;
 
 import com.hotel.booking.room.dto.RoomOptimizationRequest;
 import com.hotel.booking.room.dto.RoomOptimizationResponse;
+import com.hotel.booking.room.facade.RoomOptimizationFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class RoomOptimizationController {
 
+    private RoomOptimizationFacade roomOptimizationFacade;
+
+    @Operation(summary = "Optimizes Room Occupancy")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Room Occupancy Optimized", content =
+                    { @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = RoomOptimizationResponse.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad Request, please check the inputs", content =
+                    { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "500", description = "Internal server error",  content =
+                    { @Content(mediaType = "application/json") }) })
     @PostMapping("/optimize")
     public ResponseEntity<RoomOptimizationResponse> optimizeRooms(@Valid @RequestBody RoomOptimizationRequest roomOptimizationRequest) {
 
-        return ResponseEntity.ok().build();
+        final RoomOptimizationResponse response = roomOptimizationFacade.optimizeRooms(roomOptimizationRequest);
+
+        return ResponseEntity.ok(response);
     }
 }
